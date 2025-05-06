e967649e17f3f25561d46455b97804e0a120e72a
sanferq <proselkinmaksim@gmail.com>
2025-05-06 04:44:06 +0300

    push

--- src/utils/date.ts
+++
@@ -1,9 +0,0 @@
-export const formatTransactionDate = (date: string) => {
-  return new Date(date).toLocaleString("ru-RU", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-  })
-}
